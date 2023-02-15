f39aca024b1197a352186f27bcd3568565cecb8e
df <dufan@vspn.com>
2023-02-15 22:23:26 +0800

    好数组

---
+++ 1500-2000/1200-1300/good_array_test.go
@@ -0,0 +1,45 @@
+package leetcode
+
+import (
+	"testing"
+)
+
+func Test_isGoodArray(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "示例1：好数组",
+			args: args{
+				nums: []int{3, 5, 7, 9},
+			},
+			want: true,
+		},
+		{
+			name: "示例2：好数组,有重复数字",
+			args: args{
+				nums: []int{5, 5, 7, 9},
+			},
+			want: true,
+		},
+		{
+			name: "示例3：不是好数组",
+			args: args{
+				nums: []int{2, 4, 6},
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isGoodArray(tt.args.nums); got != tt.want {
+				t.Errorf("isGoodArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
